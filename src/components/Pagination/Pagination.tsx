11a683d62c85a31904d424a8f8cb65d4a48fcfc9
braided <leybavlad@gmail.com>
2026-09-14 15:18:24 +0200

    refactor: simplify Pagination component by removing unnecessary type imports and improving compatibility

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,15 +1,5 @@
-import ReactPaginateModule, { type ReactPaginateProps } from "react-paginate";
+import ReactPaginate from "react-paginate";
 import css from "./Pagination.module.css";
-import type { ComponentType } from "react";
-
-type ModuleWithDefault<T> = { default: T };
-
-// Коректний імпорт для сумісності з Vite 8.x.x
-const ReactPaginate = (
-  ReactPaginateModule as unknown as ModuleWithDefault<
-    ComponentType<ReactPaginateProps>
-  >
-).default;
 
 interface PaginationProps {
   onPageChange: (selectedPage: number) => void;
@@ -26,8 +16,8 @@ export default function Pagination({
     <ReactPaginate
       onPageChange={({ selected }) => onPageChange(selected)}
       forcePage={forcePage}
-      pageRangeDisplayed={3} // Кількість видимих кнопок сторінок по центру
-      marginPagesDisplayed={1} // Кількість видимих сторінок на початку і в кінці
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
       pageCount={pageCount}
       containerClassName={css.pagination}
       activeClassName={css.active}
